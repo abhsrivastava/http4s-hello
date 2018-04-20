d6524d062b4a8701c782e0daf7cec58c388d25ac
Abhishek Srivastava <abhsrivastava@researchnow.com>
2018-04-20 00:59:09 -0500

    removed duplicte imports

--- server/src/main/scala/HelloService.scala
+++
@@ -1,9 +0,0 @@
-import cats.effect._
-import org.http4s._, org.http4s.dsl.io._, org.http4s.implicits._
-
-object HelloService {
-  val service = HttpService[IO] {
-    case GET -> Root / name => 
-      Ok(s"Hello $name")
-  }
-}
--- server/src/main/scala/Main.scala
+++ server/src/main/scala/Main.scala
@@ -1,5 +1,7 @@
 import cats.effect._
-import org.http4s._, org.http4s.dsl.io._, org.http4s.implicits._
+import org.http4s._
+import org.http4s.dsl.io._
+import org.http4s.implicits._
 
 object Main extends App {
   val service = HttpService[IO] {
